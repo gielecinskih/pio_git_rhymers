38fab302fe5e50831899c8abd970f10c7e699425
Hubert Gielecinski <gielaktv@gmail.com>
2021-01-24 19:37:38 +0100

    1.5.1 main method refactor

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -10,6 +10,11 @@ class RhymersDemo {
     public static void main(String[] args) {
         Rhymersfactory factory = new DefaultRhymersFactory();
 
+        RhymersFactory(factory);
+
+    }
+
+    private static void RhymersFactory(Rhymersfactory factory) {
         defaultCountingOutRhymer[] rhymers = {factory.GetStandardRhymer(), factory.GetFalseRhymer(),
                 factory.GetFIFORhymer(), factory.GetHanoiRhymer()};
 
@@ -29,7 +34,6 @@ class RhymersDemo {
 
         System.out.println("total rejected is "
                 + ((HanoiRhymer) rhymers[3]).reportRejected());
-
     }
 
 }
