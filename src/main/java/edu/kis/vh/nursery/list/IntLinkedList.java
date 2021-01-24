1b7665c4eedbd074ed729834dd5da106b507a93d
Hubert Gielecinski <gielaktv@gmail.com>
2021-01-24 19:42:31 +0100

    class Node nested in class IntLinkedList

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -2,6 +2,15 @@ package edu.kis.vh.nursery.list;
 
 public class IntLinkedList {
 
+	public static class Node{
+		public int value;
+		public Node prev, next;
+
+		public Node(int i) {
+			value = i;
+		}
+	}
+
 	private Node last;
 	private int i;
 
@@ -43,13 +52,5 @@ public class IntLinkedList {
 
 	public void setLast(Node last) {
 		this.last = last;
-	}
-
-	public int getI() {
-		return i;
-	}
-
-	public void setI(int i) {
-		this.i = i;
 	}
 }
